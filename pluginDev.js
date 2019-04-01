f05b9302267791fa4761b0efc9d8debd5ec0260d
Sevenfield <blackpool.farr@gmail.com>
2019-04-01 15:36:52 -0400

    Update pluginDev.js

--- pluginDev.js
+++ pluginDev.js
@@ -2,16 +2,15 @@ videojs.registerPlugin('pluginDev', function() {
   var myPlayer = this;
 
 	myPlayer.one('loadedmetadata', function() {
-	  //var playlistItems = document.getElementsByClassName('vjs-playlist-thumbnail'),
-
 	  var playlistItems = document.querySelectorAll('.vjs-playlist-thumbnail img'),
+	  	vidTitle = document.querySelectorAll('.vjs-playlist-name').textContent,
 	  	playlistData = myPlayer.playlist();
 
 		function setAttr() {
 			var i,
 				iMax = playlistItems.length;
 			for (i = 0; i < iMax; i++) {
-				playlistItems[i].setAttribute('alt', 'fdhfd');
+				playlistItems[i].setAttribute('alt', vidTitle);
 			}
 		  console.log(playlistItems);
 		}
