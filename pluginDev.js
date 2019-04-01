0684d75a4a3c62e4ddb946cc56748e941dc1eb4f
Sevenfield <blackpool.farr@gmail.com>
2019-04-01 13:47:12 -0400

    Update pluginDev.js

--- pluginDev.js
+++ pluginDev.js
@@ -1,9 +1,9 @@
 videojs.registerPlugin('pluginDev', function() {
-  var player = this,
-  overlay = document.createElement('div');
-  overlay.className = 'vjs-overlay';
-  overlay.innerHTML = "dfhfdhfdhfdh";
-  player.el().appendChild(overlay);
+  var myPlayer = this:
+  // overlay = document.createElement('div');
+  // overlay.className = 'vjs-overlay';
+  // overlay.innerHTML = "dfhfdhfdhfdh";
+  // player.el().appendChild(overlay);
   playlistData = myPlayer.playlist();
   console.log(playlistData);
 });
